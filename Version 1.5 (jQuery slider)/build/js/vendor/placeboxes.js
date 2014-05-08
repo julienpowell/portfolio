3d6d925c34a1b11f1d46ceedc64ecbd511312cd0
julienpowell <julienpowell@shaw.ca>
2014-05-08 11:13:33 -0700

    first commit

---
+++ Version 1.5 (jQuery slider)/build/js/vendor/placeboxes.js
@@ -0,0 +1,16 @@
+﻿//  ########################################################################################
+//   Replaces all Image and Div elements on a page with placeholders from http://placebox.es
+//   Exclude elements by adding a class with the name: exclude
+//  ########################################################################################
+
+(function ($) {
+    $.fn.placeboxes = function () {
+        this.find('img').each(function (i, element) {
+            if (element.nodeName == 'IMG' && $(this).hasClass('exclude') == false) {
+                $(this).attr('src', 'http://placebox.es/' + $(this).width() + '/' + $(this).height() + '?ran=' + Math.floor(Math.random() * 101));
+            } else if (element.nodeName == 'DIV' && $(this).hasClass('exclude') == false) {
+                $(this).css('background-image', 'url(http://placebox.es/' + $(this).width() + '/' + $(this).height() + '?ran=' + Math.floor(Math.random() * 101) + ')');
+            }
+        });
+    };
+})(jQuery);
